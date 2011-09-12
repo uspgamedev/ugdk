f73325345008d79d82d6b5c6cf36494111802942
henriquegemignani <henrique@gemignani.org>
2011-09-12 19:15:08 -0300

    Created a deploy script for Windows.

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+@echo off
+mkdir deploy > nul
+del /F /S /Q deploy > nul
+mkdir deploy\lib > nul
+mkdir deploy\include > nul
+
+echo Copying static library files...
+
+copy lib\Release\ugdk.lib deploy\lib\ugdk.lib > nul
+copy lib\Debug\ugdk_d.lib deploy\lib\ugdk_d.lib > nul
+
+echo Copying header files...
+
+xcopy "src\ugdk\*.h" "deploy\include\ugdk\" /s /e /c > nul
+
+pause
