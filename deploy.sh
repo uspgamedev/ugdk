201947d5deb84f5415903156972b0a16b0fb2eb1
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2011-09-14 16:34:19 -0300

    Added a deploy script.

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd src
+
+directories=`find . -type d`
+headers=`find . -name *.h`
+
+cd ..
+
+for dir in $directories
+do
+    mkdir -p $1/include/$dir
+done
+
+for header in $headers
+do
+    cp src/$header $1/include/$header
+done
+
+mkdir -p $1/lib
+
+cp lib/*.a lib/*.so $1/lib
+
+
