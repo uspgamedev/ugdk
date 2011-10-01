dad9a9afe193676aabcf3478e8fe8b3b5642e682
henriquegemignani <henrique@gemignani.org>
2011-10-01 19:54:49 -0300

    More operators, methods, comments and standards. Also, the static functions use references.

--- src/ugdk/math/vector2D.cc
+++ src/ugdk/math/vector2D.cc
@@ -3,83 +3,50 @@
 
 namespace ugdk {
 
-// Member methods
-
-// Devolve a norma do vetor
-float Vector2D::length() const {
-    return sqrt((x * x) + (y * y));
+// Returns the norm-1.
+float Vector2D::NormOne() const {
+    return fabs(x) + fabs(y);
 }
-float Vector2D::angle() const {
+
+float Vector2D::Length() const {
+    return sqrt(LengthSquared());
+}
+
+float Vector2D::Angle() const {
     return atan2(y, x);
 }
 
-// Static methods
-
-// Devolve a + b
-Vector2D Vector2D::Add(Vector2D a, Vector2D b) {
-    Vector2D ret;
-    ret.x = (a.x + b.x);
-    ret.y = (a.y + b.y);
-    return ret;
-}
-
-// Devolve a - b
-Vector2D Vector2D::Subtract(Vector2D a, Vector2D b) {
-    Vector2D ret;
-    ret.x = (a.x - b.x);
-    ret.y = (a.y - b.y);
-    return ret;
-}
-
-// Devolve scalar . a
-Vector2D Vector2D::Multiply(Vector2D a, float scalar) {
-    Vector2D ret;
-    ret.x = (a.x * scalar);
-    ret.y = (a.y * scalar);
-    return ret;
-}
-
-// Devolve <a,b>
-float Vector2D::InnerProduct(Vector2D a, Vector2D b) {
-    return (a.x * b.x) + (a.y * b.y);
-}
-
-// Devolve o vetor a normalizado
-Vector2D Vector2D::Normalized(Vector2D a) {
-    if (a.length() == 0) {
-        return a;
+Vector2D Vector2D::Normalize() const {
+    float norm = Length();
+    if (norm == 0) {
+        return *this;
     }
-
-    Vector2D ret;
-    ret.x = (a.x / a.length());
-    ret.y = (a.y / a.length());
-    return ret;
+    return (*this) / norm;
 }
 
-Vector2D Vector2D::Rotate(Vector2D a, float angle) {
-    float ca = cos(angle),
-    		 sa = sin(angle);
-    Vector2D ret;
-    ret.x = a.x * ca - a.y * sa;
-    ret.y = a.x * sa + a.y * ca;
-    return ret;
+Vector2D Vector2D::Rotate(const float angle) const {
+    float ca = cos(angle), sa = sin(angle);   
+    return Vector2D(x * ca - y * sa, x * sa + y * ca);
 }
 
-//Overload de operadores
-Vector2D operator+(const Vector2D& left_value,
-                   const Vector2D& right_value) {
-    return Vector2D::Add(left_value, right_value);
+Vector2D operator+(const Vector2D &left, const Vector2D &right) {
+    return Vector2D::Add(left, right);
+}
+Vector2D operator-(const Vector2D &left, const Vector2D &right) {
+    return Vector2D::Subtract(left, right);
+}
+Vector2D operator*(const float  &scalar, const Vector2D &right) {
+    return Vector2D::Multiply(right, scalar);
+}
+Vector2D operator*(const Vector2D &left, const float   &scalar) {
+    return Vector2D::Multiply(left, scalar);
+}
+Vector2D operator/(const Vector2D &left, const float   &scalar) {
+    return Vector2D::Multiply(left, 1.0f / scalar);
 }
 
-Vector2D operator-(const Vector2D& left_value,
-                   const Vector2D& right_value) {
-    return Vector2D::Subtract(left_value, right_value);
-}
-
-
-Vector2D operator*(const Vector2D& left_value,
-                   const float& scalar) {
-    return Vector2D::Multiply(left_value, scalar);
+float operator*(const Vector2D &left, const Vector2D &right) {
+    return Vector2D::InnerProduct(left, right);
 }
 
 }  // namespace framework
--- src/ugdk/math/vector2D.h
+++ src/ugdk/math/vector2D.h
@@ -3,38 +3,79 @@
 
 namespace ugdk {
 
-// Vetores de 2 dimensoes.
-// As entradas do vetor sao valores float.
+// 2 dimension vectors, using floats.
 class Vector2D {
   public:
-    // Construtores e destrutores
-    Vector2D() : x(0.0), y(0.0) { }
-    Vector2D(float x, float y) : x(x), y(y) { }
+    // Initializes both fields with 0.0f
+    Vector2D() : x(0.0f), y(0.0f) {}
+
+    // Initializes both fields with val.
+    Vector2D(float val) : x(val), y(val) {}
+
+    // Initializes both fields with given values.
+    Vector2D(float x, float y) : x(x), y(y) {}
+
     ~Vector2D() { }
 
+    // Fields are accessible as x/y or val[0] and val[1].
+    union {
+        struct { float x, y;   };
+        struct { float val[2]; };
+    };
 
-    // Member methods
-    float length() const;
-    float angle() const;
+    // Returns the norm-1.
+    float NormOne() const;
+
+    // Returns the norm-2.
+    float Length() const;
+    
+    // Returns the norm-2 squared.
+    float LengthSquared() const { return (x*x) + (y*y); }
+
+    // Returns the angle of this vector.
+    float Angle() const;
+
+    // Returns a new vector equivalent to this vector normalized.
+    Vector2D Normalize() const;
+
+    // Returns a new vector equivalent to this vector rotated "angle" counter-clockwise.
+    Vector2D Rotate(const float angle) const;
 
     // Static methods
-    static Vector2D Add(Vector2D a, Vector2D b);
-    static Vector2D Subtract(Vector2D a, Vector2D b);
-    static Vector2D Multiply(Vector2D a, float scalar);
-    static float InnerProduct(Vector2D a, Vector2D b);
-    static Vector2D Normalized(Vector2D a);
-    static Vector2D Rotate(Vector2D a, float angle);
+    static Vector2D Add(const Vector2D &a, const Vector2D &b) {
+        return Vector2D(a.x + b.x, a.y + b.y);
+    }
+    static Vector2D Subtract(const Vector2D &a, const Vector2D &b) {
+        return Vector2D(a.x - b.x, a.y - b.y);
+    }
+    static Vector2D Multiply(const Vector2D &a, const float &scalar) {
+        return Vector2D(a.x * scalar, a.y * scalar);
+    }
+    static float InnerProduct(const Vector2D &a, const Vector2D &b) {
+        return (a.x * b.x) + (a.y * b.y);
+    }
 
-    float x, y;
+    // Deprecated. Use the upper case method.
+    float length() const { return Length(); }
+	
+	// Deprecated. Use the upper case method.
+	float angle() const { return Angle(); }
+
+    // Deprecated. Use the method function Normalize.
+    static Vector2D Normalized(Vector2D &a) { return a.Normalize(); }
+
+    // Deprecated. Use the method function Rotate.
+    static Vector2D Rotate(Vector2D &a, float angle) {
+        return a.Rotate(angle);
+    }
 };
 
-	// Overload de operadores
-	Vector2D operator+(const Vector2D &left_value,
-                   const Vector2D &right_value);
-	Vector2D operator-(const Vector2D& left_value,
-                   const Vector2D& right_value);
-	Vector2D operator*(const Vector2D& left_value,
-                   const float& scalar);
+Vector2D operator+(const Vector2D &left, const Vector2D &right);
+Vector2D operator-(const Vector2D &left, const Vector2D &right);
+Vector2D operator*(const float  &scalar, const Vector2D &right);
+Vector2D operator*(const Vector2D &left, const float   &scalar);
+Vector2D operator/(const Vector2D &left, const float   &scalar);
+float    operator*(const Vector2D &left, const Vector2D &right);
 
 }  // namespace framework
 
