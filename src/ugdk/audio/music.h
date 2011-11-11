908d2a56dfbb6434bea7b17b267df9a6685a848b
Renan <renanteruoc@gmail.com>
2011-11-11 16:18:56 -0200

    Documentação simples pro music.

--- src/ugdk/audio/music.h
+++ src/ugdk/audio/music.h
@@ -10,15 +10,26 @@ namespace ugdk {
 class AudioManager;
 
 // Musica
+/**@class Music
+ *Note: This class cannot be directly instantiated. Please use AudioManager's LoadMusic.
+ *@see AudioManager
+ */
 class Music {
   public:
+    ///Plays the music once.
     void Play(); // toca musica uma vez
+    ///Plays the music the given number of times.
     void Play(int loops); // toca musica loops vezes
+    ///Plays the music until told to stop.
     void PlayForever(); // toca musica infinitas vezes
+    ///Stops playing hte music.
     void Stop(); // para musica
+    ///Return whether the music is playing or not.
     bool IsPlaying(); // esta musica esta' tocando?
 
+    ///Sets the volume. 0 is quiet, 1 is the full volume.
     void SetVolume(float vol); // 0.0f (quiet) <= vol <= 1.0f (loud)
+    ///Returns the music's curretn volume, between 0 and 1.
     float Volume(); // obtem volume, 0.0f <= volume <= 1.0f
 
   private:
