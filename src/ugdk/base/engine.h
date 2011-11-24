a2004af975bf70840dc1bdc8aeec87f8f97d2d92
Renan Teruo Carneiro <renanteruoc@gmail.com>
2011-11-24 17:19:52 -0200

    Documentação pro Engine. Pode estar porca.

--- src/ugdk/base/engine.h
+++ src/ugdk/base/engine.h
@@ -24,45 +24,98 @@ class Scene;
 
 typedef gdd::CachedLoader<AnimationSet> AnimationLoader;
 
+/// The game engine. Manages scenes, time, and the Audio, Video, Input and Text managers.
+/** Engine is a singleton. Use Engine::reference to get a pointer to an engine.
+ * @see reference()
+ */
 class Engine {
   public:
     // Engine e' um singleton. Use Engine::reference()
     // para obter um ponteiro para o objeto desta classe.
+    /// Returns a pointer to the current Engine. Creates an Engine if there isn't one.
     static Engine* reference() { static Engine *r = NULL;
                                  return r ? r : r = new Engine; }
 
+    /// Returns a reference to the Video Manager.
+    /** @see VideoManager
+     */
     VideoManager *video_manager() { return video_manager_; }
+    /// Returns a reference to the Input Manager.
+    /** @see InputManager
+     */
     InputManager *input_manager() { return input_manager_; }
+    /// Returns a reference to the Time Handler.
+    /** @see TimeHandler
+     */
     TimeHandler *time_handler() { return time_handler_; }
+    /// Returns a reference to the Audio Manager.
+    /** @see AudioManager
+     */
     AudioManager *audio_manager() { return audio_manager_; }
+    /// Returns a reference to the Text Manager.
+    /** @see TextManager
+     */
     TextManager *text_manager() { return text_manager_; }
-	PathManager *path_manager() { return path_manager_; }
+    /// Returns a reference to the Path Manager.
+    /** @see PathManager
+     */
+    PathManager *path_manager() { return path_manager_; }
 
-	AnimationLoader& animation_loader() { return animation_loader_; }
+    /// Returns a reference to the Animation Loader.
+    /** @see AnimationLoader
+     */
+    AnimationLoader& animation_loader() { return animation_loader_; }
+
+    /// Returns the window dimensions.
     Vector2D window_size();
 
     // Gerenciamento do motor
+    /**@name Engine Management
+     * @{
+     */
+    /// Initializes the engine. Returns true if sucessful, false otherwise.
+    /** @param windowTitle The window title.
+     * @param windowSize The window's dimensions.
+     */
 	bool Initialize(std::string windowTitle, Vector2D windowSize, 
 		bool fullscreen,
 		std::string base_path = std::string("./"),
 		std::string icon = std::string());
-
+    /// Starts running the engine.
     void Run();
+    /// Stops and frees the engine.
     void Release();
 
+    /** @}
+     */
+
+    /** @name Scene Management
+     * @{
+     */
     // Gerenciamento de cenas
+
+    /// Puts a scene onto the scene list, on top.
+    /** @param scene The scene to be put.
+     */
     void PushScene(Scene* scene);
+    /// Returns the current scene.
     Scene* CurrentScene() const;
+    /// Removes the top scene.
     void PopScene();
 
     // TODO: SERIOUSLY FIX THIS. Big fat ugly code to allow some special 
     // layers to be unnafected by the light system. Meant for user interface.
+    /// Pushes a layer unaffected by the light system. Meant for interfaces.
     void PushInterface(Layer* layer);
+    /// Removes a layer pushed by the above function.
     void RemoveInterface(Layer *layer);
-
+    /** @}
+     */
+    /// Returns the current running FPS.
     uint32 current_fps() { return reported_fps_; }
 
     // Saida do motor
+    /// Stops the engine and clears the scene list.
     void quit() { quit_ = true; }
 
     // outros
