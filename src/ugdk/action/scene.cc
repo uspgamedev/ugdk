39d3937629c60a17451adad5ae1c5d92f0798d2e
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2011-12-18 03:25:16 -0200

    Scenes now has a background music!

--- src/ugdk/action/scene.cc
+++ src/ugdk/action/scene.cc
@@ -1,4 +1,7 @@
 #include <ugdk/action/scene.h>
+#include <ugdk/base/engine.h>
+#include <ugdk/audio/audiomanager.h>
+#include <ugdk/audio/music.h>
 
 namespace ugdk {
 
@@ -10,6 +13,19 @@ Scene::~Scene() {
     while (it != layers_.end()) {
         delete (*it);
         ++it;
+    }
+}
+
+void Scene::Focus() {
+    if(background_music_ != NULL) { 
+        if(!background_music_->IsPlaying())
+            background_music_->PlayForever();
+        else
+            background_music_->Unpause();
+    } else if(stops_previous_music_) {
+        Music* current_music = AUDIO_MANAGER()->CurrentMusic();
+        if(current_music != NULL)
+            current_music->Stop();
     }
 }
 
@@ -43,6 +59,11 @@ void Scene::RenderLight() {
             ++it;
         }
     }
+}
+
+void Scene::End() {
+    if(background_music_ != NULL)
+        background_music_->Pause();
 }
 
 }
--- src/ugdk/action/scene.h
+++ src/ugdk/action/scene.h
@@ -6,10 +6,7 @@
 
 namespace ugdk {
 
-// Classe Scene
-// Representa uma CENA, que contem varias camadas de graficos.
-// Uma cena e' uma "tela" inteira do jogo, como por exemplo o main menu,
-// ou o jogo em si (onde ocorre a acao).
+class Music;
 
 /**
    @class Scene
@@ -21,62 +18,73 @@ namespace ugdk {
 */
 class Scene {
   public:
-    Scene() : finished_(false), visible_(true) {}
+    Scene() : finished_(false), visible_(true), background_music_(NULL), stops_previous_music_(true) {}
   /**
      Note: Destroys all layers in the scene.
   */
     virtual ~Scene();
 
-    // Funcoes basicas pra adicionar e remover layers.
+    /// Method called when this Scene arrives on the top of the Scene stack.
+    virtual void Focus();
+
+    /// Method called when this Scene leaves the top of the Scene stack.
+    virtual void DeFocus() {}
+
     /// Adds a Layer to the scene on top of current layers.
     void AddLayer(Layer *layer) { layers_.push_back(layer); };
     /// Removes the specified Layer from the scene.
     void RemoveLayer(Layer *layer) { layers_.remove(layer); };
 
-    // Termina a cena.
     /// Finishes the scene.
     void Finish() { End(); finished_ = true; }
 
-    // Atualiza a logica da cena.
     /// Logical update of the scene.
     /**
        @param delta_t Time in seconds since last update
     */
     virtual void Update(float delta_t);
 
-    // Redesenha o grafico da cena.
     /// Redraws the scene's graphics.
     void Render();
 
-    // Redesenha as fontes de luz.
-    /// Redraws lightsources.
+    /// Redraws the lightsources.
     void RenderLight();
 
-    // Getters.
+    /// Whether this scene stops the previous music even if wont play any music.
+    void StopsPreviousMusic(bool set) { stops_previous_music_ = set; }
+
     /** @name Getters and Setters
 	@{
     */
+    bool finished() const { return finished_; }
 
-    bool finished() { return finished_; }
-
-    bool visible () { return visible_; }
+    bool visible () const { return visible_; }
     void set_visible (bool set) { visible_ = set; }
+
+    Music* background_music() const { return background_music_; }
+    void set_background_music(Music* music) { background_music_ = music; }
     /**@}
      */
   protected:
 
-    // Encerra a atividade da cena.
     /// Ends the scene activity.
-    virtual void End() {}
+    virtual void End();
 
-    // Lista das camadas.
     /// Layer list
     ::std::list<Layer*> layers_;
-    // Bool que diz se a cena esta' terminada ou nao.
+
     /// Tells whether scene is finished or not.
     bool finished_;
+
     /// Tells whether scene is visible or not.
     bool visible_;
+
+    /// The background music when this scene is on top.
+    Music* background_music_;
+
+  private:
+    /// Whether this scene stops the previous music even if wont play any music.
+    bool stops_previous_music_;
 
 }; // class Scene.
 
