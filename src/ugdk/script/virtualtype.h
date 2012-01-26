61397b3bb05e4946e17549ab393f08ad7bcf610f
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-01-26 17:15:43 -0200

    Adding some files just to create the script folder.

---
+++ src/ugdk/script/virtualtype.h
@@ -0,0 +1,22 @@
+
+#ifndef UGDK_SCRIPT_VIRTUALTYPE_H_
+#define UGDK_SCRIPT_VIRTUALTYPE_H_
+
+namespace ugdk {
+
+class VirtualType {
+public:
+    ~VirtualType() {
+        // TODO Auto-generated destructor stub
+    }
+
+protected:
+    VirtualType() {
+        // TODO Auto-generated constructor stub
+
+    }
+};
+
+} /* namespace ugdk */
+
+#endif /* UGDK_SCRIPT_VIRTUALTYPE_H_ */
