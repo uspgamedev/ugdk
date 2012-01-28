db0ce19d21ef31a82c8768552595e30d913d8dd7
Fernando Omar Aluani <fernando_aluani@hotmail.com>
2012-01-28 18:12:08 -0200

    Implemented VO operators; Implemented VData virtual operators (function prototype);

--- src/ugdk/script/virtualdata.h
+++ src/ugdk/script/virtualdata.h
@@ -2,10 +2,14 @@
 #ifndef UGDK_SCRIPT_VIRTUALDATA_H_
 #define UGDK_SCRIPT_VIRTUALDATA_H_
 
+#include <vector>
+#include <string>
+
 namespace ugdk {
 
 namespace script {
 
+class VirtualObj;
 class VirtualType;
 
 /// Abstract class representing virtual script data.
@@ -19,12 +23,16 @@ class VirtualData {
     virtual ~VirtualData() {}
 
     /// Tries to unwrap the data contained in this object using the given type.
-    void* Unwrap(const VirtualType& type) = 0;
+    virtual void* Unwrap(const VirtualType& type) = 0;
 
     /// Tries to wrap the given data with the given type into this object.
-    void Wrap(void* data, const VirtualType& type) = 0;
+    virtual void Wrap(void* data, const VirtualType& type) = 0;
 
+	/// Tries to execute ourselves as a function in a script language, passing the given arguments and returning the result.
+	virtual Ptr Execute(std::vector<const VirtualObj&> args);
 
+	/// Tries to get a attribute with the given name from this object.
+	virtual Ptr GetAttr(const std::string attr_name);
 
   protected:
 
