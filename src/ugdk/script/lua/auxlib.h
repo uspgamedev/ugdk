df0bbcd4ec5b8daa83bb7315bd3d877718689700
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-01-30 20:30:30 -0200

    Adding script Lua implementation.

---
+++ src/ugdk/script/lua/auxlib.h
@@ -0,0 +1,45 @@
+
+#ifndef UGDK_SCRIPT_LUA_AUXLIB_H_
+#define UGDK_SCRIPT_LUA_AUXLIB_H_
+
+#include <functional>
+
+#include <ugdk/script/lua/header.h>
+
+namespace ugdk {
+namespace script {
+namespace lua {
+
+class State;
+
+class AuxLib {
+
+  public:
+
+    explicit AuxLib(const StatePtr& L) : L_(L) {}
+
+    static StatePtr newstate() {
+        StatePtr L(luaL_newstate());
+        return L;
+    }
+
+    void openlibs() { luaL_openlibs(L_.get()); }
+
+    const Constant loadfile(const char* filename) {
+        return Constant(
+            std::tr1::bind(luaL_loadfile, L_.get(), filename)
+        );
+    }
+
+  private:
+
+    StatePtr L_;
+
+};
+
+} /* namespace lua */
+} /* namespace script */
+} /* namespace ugdk */
+
+
+#endif /* UGDK_SCRIPT_LUA_AUXLIB_H_ */
---
+++ src/ugdk/script/lua/state.h
@@ -0,0 +1,82 @@
+
+#ifndef UGDK_SCRIPT_LUA_STATE_H_
+#define UGDK_SCRIPT_LUA_STATE_H_
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <string>
+#include <functional>
+
+#include <ugdk/script/lua/header.h>
+#include <ugdk/script/lua/auxlib.h>
+
+namespace ugdk {
+namespace script {
+namespace lua {
+
+class State {
+
+  public:
+
+    State () :
+      L_(AuxLib::newstate(), lua_close) {
+        if (L_.get() == NULL)
+          State::errormsg("Could not create Lua state.");
+    }
+
+    AuxLib aux() const { return AuxLib(L_); }
+
+    int gettop() const { return lua_gettop(L_.get()); }
+
+    void pushvalue (int index) { lua_pushvalue(L_.get(), index); }
+    void pushnil () { lua_pushnil(L_.get()); }
+
+    void push (bool b) { lua_pushboolean(L_.get(), b); }
+    void push (lua_Integer integer) { lua_pushinteger(L_.get(), integer); }
+    void push (lua_Number number) { lua_pushnumber(L_.get(), number); }
+    void push (void *ptr) { lua_pushlightuserdata(L_.get(), ptr); }
+    void push (const char* str) { lua_pushstring(L_.get(), str); }
+    void push (lua_CFunction func, int n = 0) {
+        lua_pushcclosure(L_.get(), func, n);
+    }
+
+    void pop (int n) { lua_pop(L_.get(), n); }
+
+    void insert (int index) { lua_insert(L_.get(), index); }
+    void remove (int index) { lua_remove(L_.get(), index); }
+
+    bool isnil (int index) { return lua_isnil(L_.get(), index); }
+    bool istable (int index) { return lua_istable(L_.get(), index); }
+
+    const char* tostring(int n) { return lua_tostring(L_.get(), n); }
+    int type (int n) { return lua_type(L_.get(), n); }
+    
+    void call (int nargs, int nres) { lua_call(L_.get(), nargs, nres); }
+    const Constant pcall (int nargs, int nres, int errfunc) {
+        return Constant(
+            std::tr1::bind(lua_pcall, L_.get(), nargs, nres, errfunc)
+        );
+    }
+
+    int gc (Constant what, int data) {
+        return lua_gc(L_.get(), what.value(), data);
+    }
+
+  private:
+
+    // TODO: make-do for now.
+    static void errormsg (std::string msg) {
+      fprintf(stderr, "lua: %s\n", msg.c_str());
+    }
+
+    StatePtr L_;
+
+};
+
+} /* namespace lua */
+} /* namespace script */
+} /* namespace ugdk */
+
+#endif /* UGDK_SCRIPT_LUA_STATE_H_ */
+
