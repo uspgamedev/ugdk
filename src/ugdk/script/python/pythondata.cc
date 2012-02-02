16d5d3fa4a0f03e2a5c18a1cedbb8e29d55476d3
Rewasvat <fernando_aluani@hotmail.com>
2012-02-02 19:06:34 -0200

    renamed script system Python implementation classes to remove virtual from their names;

--- src/ugdk/script/python/pythondata.cc
+++ src/ugdk/script/python/pythondata.cc
@@ -1,4 +1,4 @@
-#include <ugdk/script/python/pythonvirtualdata.h>
+#include <ugdk/script/python/pythondata.h>
 #include <ugdk/script/scriptmanager.h>
 #include <ugdk/script/virtualobj.h>
 #include <ugdk/script/swig/swigpyrun.h>
@@ -12,7 +12,7 @@ namespace python {
 
 using std::tr1::shared_ptr;
 
-void* PythonVirtualData::Unwrap(const VirtualType& type) const {
+void* PythonData::Unwrap(const VirtualType& type) const {
 	void* pointer;
 	int res = SWIG_ConvertPtrAndOwn(py_data_, &pointer, type.FromLang(LANG(Python)), 0, NULL);
 	if (SWIG_IsOK(res))
@@ -21,7 +21,7 @@ void* PythonVirtualData::Unwrap(const VirtualType& type) const {
 }
 
 /// Tries to wrap the given data with the given type into this object.
-void PythonVirtualData::Wrap(void* data, const VirtualType& type) {
+void PythonData::Wrap(void* data, const VirtualType& type) {
     if (py_data_ != NULL && own_ref_) {
         Py_DECREF(py_data_);
         py_data_ = NULL;
@@ -33,7 +33,7 @@ void PythonVirtualData::Wrap(void* data, const VirtualType& type) {
 	  So our PyVData needs to handle it.*/
 }
 
-void PythonVirtualData::Wrap(const char* str) {
+void PythonData::Wrap(const char* str) {
     if (str == NULL)    return;
     if (py_data_ != NULL && own_ref_) {
         Py_DECREF(py_data_);
@@ -44,12 +44,12 @@ void PythonVirtualData::Wrap(const char* str) {
     own_ref_ = true;
 }
 
-void PythonVirtualData::Wrap(bool boolean) {
+void PythonData::Wrap(bool boolean) {
     py_data_ = PyBool_FromLong(static_cast<long>(boolean));
     own_ref_ = true;
 }
 
-void PythonVirtualData::Wrap(int number) {
+void PythonData::Wrap(int number) {
     if (py_data_ != NULL && own_ref_) {
         Py_DECREF(py_data_);
         py_data_ = NULL;
@@ -59,7 +59,7 @@ void PythonVirtualData::Wrap(int number) {
     own_ref_ = true;
 }
 
-void PythonVirtualData::Wrap(double number) {
+void PythonData::Wrap(double number) {
     if (py_data_ != NULL && own_ref_) {
         Py_DECREF(py_data_);
         py_data_ = NULL;
@@ -69,13 +69,13 @@ void PythonVirtualData::Wrap(double number) {
     own_ref_ = true;
 }
 
-LangWrapper* PythonVirtualData::wrapper () const {
+LangWrapper* PythonData::wrapper () const {
 	return ScriptManager::ref()->GetWrapper("Python");
 }
 
 /// Tries to execute ourselves as a function in a script language,
 /// passing the given arguments and returning the result.
-VirtualData::Ptr PythonVirtualData::Execute(std::vector<Ptr> args) {
+VirtualData::Ptr PythonData::Execute(std::vector<Ptr> args) {
 	if (!PyCallable_Check(py_data_)) {
 		//The object we contain is not callable - cannot execute it.
 		return VirtualData::Ptr();
@@ -92,7 +92,7 @@ VirtualData::Ptr PythonVirtualData::Execute(std::vector<Ptr> args) {
 	// OBS.: steals the reference to object;  returns 0 on success
 	for (unsigned int i = 0; i < args.size(); i++) {
 		//check appropriate type of args[i]
-		PythonVirtualData* py_arg = static_cast<PythonVirtualData*>(args[i].get());
+		PythonData* py_arg = static_cast<PythonData*>(args[i].get());
 		if (py_arg->own_ref_) {
 			//this VData arg owns the ref to the Object it's pointing at. And since PyTuple_SetItem steals that reference...
 			py_arg->own_ref_ = false;
@@ -107,7 +107,7 @@ VirtualData::Ptr PythonVirtualData::Execute(std::vector<Ptr> args) {
 	PyObject* result = PyObject_CallObject(py_data_, arglist); //return is new ref
 	/*If result = NULL, CallObject failed (somehow), and due to the way we treat VirtualData in the system, 
 	  this is the way to treat that.*/
-	VirtualData::Ptr vdata( new PythonVirtualData(result, true) ); //PyVirtualData takes care of the ref.
+	VirtualData::Ptr vdata( new PythonData(result, true) ); //PyVirtualData takes care of the ref.
 	
 	//Before returning, kill the argument tuple we created.
 	Py_XDECREF(arglist);  //WARNING: in theory, this is correct. However in my testing (in the prototype) this cause some serious shit.
@@ -116,8 +116,8 @@ VirtualData::Ptr PythonVirtualData::Execute(std::vector<Ptr> args) {
 }
 
 /// Tries to get a attribute with the given name from this object.
-VirtualData::Ptr PythonVirtualData::GetAttribute(Ptr key) {
-    PythonVirtualData* vkey = static_cast<PythonVirtualData*>(key.get());
+VirtualData::Ptr PythonData::GetAttribute(Ptr key) {
+    PythonData* vkey = static_cast<PythonData*>(key.get());
     PyObject* key_data = vkey->py_data_;
 	PyObject* attr;
 	
@@ -139,14 +139,14 @@ VirtualData::Ptr PythonVirtualData::GetAttribute(Ptr key) {
 	}
 	/*If Py_GetAttr or Py_GetItem failed somehow, they will return null.*/
 	
-	VirtualData::Ptr vdata( new PythonVirtualData(attr, true) );
+	VirtualData::Ptr vdata( new PythonData(attr, true) );
 	return vdata;
 }
 
-void PythonVirtualData::SetAttribute(Ptr key, Ptr value) {
-    PythonVirtualData* vkey = static_cast<PythonVirtualData*>(key.get());
+void PythonData::SetAttribute(Ptr key, Ptr value) {
+    PythonData* vkey = static_cast<PythonData*>(key.get());
     PyObject* key_data = vkey->py_data_;
-    PythonVirtualData* object = static_cast<PythonVirtualData*>(value.get());
+    PythonData* object = static_cast<PythonData*>(value.get());
     PyObject* object_data = object->py_data_;
 
     /*First we try checking if our object has a attribute or string with the given
