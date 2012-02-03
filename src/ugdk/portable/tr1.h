9e5d67198684bee06a6741b18a571d12153f6c4f
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-02-03 00:38:40 -0200

    Eclipse include tr1 workaround created.

--- src/ugdk/portable/tr1.h
+++ src/ugdk/portable/tr1.h
@@ -2,7 +2,9 @@
 #ifndef UGDK_PORTABLE_TR1_H_
 #define UGDK_PORTABLE_TR1_H_
 
-#ifdef ECLIPSE_BOOST_WORKAROUND_INCLUDE
+#include <ugdk/config/config.h>
+
+#ifdef ECLIPSE_TR1_WORKAROUND_INCLUDE
 #define FROM_TR1(header) <boost/tr1/header.hpp>
 #else
 #define FROM_TR1(header) <header>
