0131d38915a0b2da3c6249b70a3d4d6d76549bac
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-02-04 15:05:12 -0200

    Lua for all of you!

--- src/ugdk/script/lua/luawrapper.cc
+++ src/ugdk/script/lua/luawrapper.cc
@@ -4,6 +4,7 @@
 
 #include <ugdk/script/virtualdata.h>
 #include <ugdk/script/virtualobj.h>
+#include <ugdk/script/lua/header.h>
 #include <ugdk/script/lua/luadata.h>
 
 namespace ugdk {
@@ -45,8 +46,18 @@ VirtualData::Ptr LuaWrapper::NewData() {
     return VirtualData::Ptr(new LuaData(this));
 }
 
-VirtualObj LuaWrapper::LoadModule(const std::string& name) {
-    return VirtualObj(NewData()); // TODO
+VirtualObj LuaWrapper::LoadModule(const string& name) {
+    string fullpath = name + "." + file_extension();
+    {
+        Gear g(MakeGear());
+        const Constant result = g.DoFile(fullpath.c_str());
+        if(result != Constant::OK())
+            return VirtualObj(VirtualData::Ptr()); // error
+        LuaData* lua_data = new LuaData(this);
+        g.SetData(lua_data->id());
+        VirtualData::Ptr data(lua_data);
+        return VirtualObj(data);
+    }
 }
 
 } /* namespace lua */
