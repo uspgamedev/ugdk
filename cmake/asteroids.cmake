7adfd7e6ff925f47ace421c5214442b7b9bc8226
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-03-14 17:49:44 -0300

    Trying to fix asteroids project in win.

--- cmake/asteroids.cmake
+++ cmake/asteroids.cmake
@@ -3,10 +3,10 @@ IF (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     SET(GAME_SRCS ${GAME_SRCS} src/mac/SDLMain.h src/mac/SDLMain.m)
 ENDIF ()
 add_executable (asteroids EXCLUDE_FROM_ALL examples/asteroids-script/src/asteroids.cc
-					   examples/asteroids-script/src/scriptscene.cc
-					   examples/asteroids-script/src/scriptentity.cc
-					   examples/asteroids-script/src/scriptentitystack.cc
-                       ${GAME_SRCS})
+					      examples/asteroids-script/src/scriptscene.cc
+					      examples/asteroids-script/src/scriptentity.cc
+					      examples/asteroids-script/src/scriptentitystack.cc
+                ${GAME_SRCS})
 
 set_target_properties (asteroids PROPERTIES RUNTIME_OUTPUT_DIRECTORY examples/asteroids-script/)
 
@@ -18,7 +18,14 @@ else ()
 endif (CMAKE_COMPILER_IS_GNUCXX)
 
 # FIXME
-target_link_libraries (asteroids 
-                                           ${FRAMEWORK_BUILDNAME}
-					   ${LUA_LIBRARIES}
-					   ${PYTHON_FINAL_LIB})
+target_link_libraries (asteroids
+  ${FRAMEWORK_BUILDNAME}
+  ${EXTRA_LIBRARIES}
+  ${SDL_LIBRARY}
+  ${SDLMIXER_LIBRARY}
+  ${SDLTTF_LIBRARY}
+  ${SDLIMAGE_LIBRARY}
+  ${OPENGL_LIBRARIES}
+  ${UGDK_LIBRARIES})
+  #${LUA_LIBRARIES}
+  #${PYTHON_FINAL_LIB})
