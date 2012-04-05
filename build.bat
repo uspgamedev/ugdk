9e60d86fa7348c5143d6db571d8ebdcc80e30989
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-04-04 22:08:29 -0300

    build.bat now ignores asteroids.

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
+@echo off
 call "%VS100COMNTOOLS%vsvars32.bat"
+
+echo Running CMake for DEBUG
 cmake -DLINK_TYPE=STATIC -DBUILD_TYPE=DEBUG .
-msbuild ugdk.sln /p:Configuration=Debug
+msbuild ugdk_d.vcxproj /p:Configuration=Debug
+
+echo Running CMake for RELEASE
 cmake -DLINK_TYPE=STATIC -DBUILD_TYPE=RELEASE .
-msbuild ugdk.sln /p:Configuration=Release
+msbuild ugdk.vcxproj /p:Configuration=Release
+
 pause
