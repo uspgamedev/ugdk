e05c99a7d297954ad195becc62f34618027b1cb4
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-04-08 02:01:14 -0300

    Build.bat updated, and /MP flag for MSVC.

--- build.bat
+++ build.bat
@@ -1,12 +1,9 @@
 @echo off
 call "%VS100COMNTOOLS%vsvars32.bat"
 
-echo Running CMake for DEBUG
-cmake -DLINK_TYPE=STATIC -DBUILD_TYPE=DEBUG .
-msbuild ugdk_d.vcxproj /p:Configuration=Debug
+if not exist ugdk.vcxproj (cmake -DLINK_TYPE=STATIC .)
 
-echo Running CMake for RELEASE
-cmake -DLINK_TYPE=STATIC -DBUILD_TYPE=RELEASE .
+msbuild ugdk.vcxproj /p:Configuration=Debug
 msbuild ugdk.vcxproj /p:Configuration=Release
 
 pause
