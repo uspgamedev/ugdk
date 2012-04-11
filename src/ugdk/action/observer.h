cfb28193b60714714940ac82b4736aace40c752d
Renan Teruo Carneiro <renanteruoc@gmail.com>
2012-04-11 18:07:01 -0300

    observer.h is now in namespace action

--- src/ugdk/action/observer.h
+++ src/ugdk/action/observer.h
@@ -1,10 +1,15 @@
 #ifndef HORUSEYE_FRAMEWORK_OBSERVER_H_
 #define HORUSEYE_FRAMEWORK_OBSERVER_H_
 namespace ugdk{
+
+namespace action{
 class Observer{
   public:
     virtual void Tick() = 0;
 };
-}
 
-#endif
+} /* namespace action */
+
+} /* namespace ugdk */
+
+#endif /* HORUSEYE_FRAMEWORK_OBSERVER_H_ */
