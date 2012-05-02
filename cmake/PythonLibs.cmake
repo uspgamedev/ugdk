4766934463896e501a7fdf3a9a09df9f987b88fb
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-05-02 00:46:13 -0300

    Correctly link to Lua and Python libs...

--- cmake/PythonLibs.cmake
+++ cmake/PythonLibs.cmake
@@ -4,11 +4,8 @@ find_package (PythonLibs 2.7)
 if (PYTHONLIBS_FOUND)
 
   include_directories (${PYTHON_INCLUDE_DIRS})
+  LIST(APPEND UGDK_SRC              ${UGDK_PYTHON_SRC})
   ugdk_add_scriptlang (python)
-
-  LIST(APPEND UGDK_SRC              ${UGDK_PYTHON_SRC}) 
-  LIST(APPEND UGDK_LIBRARIES        ${PYTHON_LIBRARIES})
-  LIST(APPEND UGDK_LIBRARIES_DEBUG  ${PYTHON_DEBUG_LIBRARY})
 
   message ("-- Python modules added.")
     
