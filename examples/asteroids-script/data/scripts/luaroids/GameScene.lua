f56fc71bd1acc3b5239d06758a5734d17077e1a8
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-05-02 20:08:25 -0300

    Hummm... deep erros.

--- examples/asteroids-script/data/scripts/luaroids/GameScene.lua
+++ examples/asteroids-script/data/scripts/luaroids/GameScene.lua
@@ -1,15 +1,32 @@
 
 require "ugdk.base"
 require "ugdk.action"
+require "ugdk.input"
 
 local AsteroidsScene = {}
+
+print(getmetatable(SWIG.Scene[".fn"]))
+
+for k,v in pairs(SWIG.Scene[".fn"]) do
+  AsteroidsScene[k] = v
+end
 
 function AsteroidsScene:Focus () end
 function AsteroidsScene:DeFocus () end
 function AsteroidsScene:End () end
 
+function AsteroidsScene:CheckCommands ()
+  local input = ugdk_base.Engine_reference():input_manager()
+  if input:KeyPressed(ugdk_input.K_ESCAPE) then
+    SWIG.Scene[".fn"].Finish(self)
+  elseif input:KeyPressed(ugdk_input.K_HOME) then
+    --self:Finish()
+    --StartupScene()
+  end
+end
+
 function AsteroidsScene:Update (dt)
-  
+  self:CheckCommands()
 end
 
 setmetatable(AsteroidsScene, SWIG.Scene)
