a743ce5d81cc6206a93618b6330af4eff7537194
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-05-04 19:13:29 -0300

    Separated Task and GenericTask.

--- src/ugdk/action/task.h
+++ src/ugdk/action/task.h
@@ -1,8 +1,6 @@
 #ifndef UGDK_ACTION_TASK_H_
 #define UGDK_ACTION_TASK_H_
 
-#include <functional>
-
 namespace ugdk {
 
 namespace action {
@@ -22,24 +20,6 @@ class Task {
   protected:
     bool finished_;
     int priority_;
-};
-
-template <class Data>
-class GenericTask : public Task {
-  typedef Task super;
-  protected:
-    Data data_;
-    std::tr1::function<bool (double, Data&)> function_;
-  public:
-    GenericTask(std::tr1::function<bool (double, Data&)> func, const Data& data) 
-        : function_(func), data_(data) {}
-
-    GenericTask(std::tr1::function<bool (double, Data&)> func, const Data& data, int priority) 
-        : Task(priority), function_(func), data_(data) {}
-
-    void operator()(double dt) {
-        finished_ = !function_(dt, data_);
-    }
 };
 
 } /* namespace action */
