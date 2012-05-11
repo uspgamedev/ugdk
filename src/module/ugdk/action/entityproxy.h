fdf58dde8a53c0065ad0a86153e948e7fba2475f
Rewasvat <fernando_aluani@hotmail.com>
2012-05-11 18:07:37 -0300

    Fixed swig/python memory leak in EntityProxy

--- src/module/ugdk/action/entityproxy.h
+++ src/module/ugdk/action/entityproxy.h
@@ -4,6 +4,7 @@
 #include <ugdk/action/entity.h>
 #include <ugdk/script/baseproxy.h>
 #include <list>
+#include <module/ugdk/action/sceneproxy.h>
 
 namespace ugdk {
 namespace action {
@@ -23,10 +24,18 @@ public:
     }
 
     virtual void OnSceneAdd(Scene* scene) {
-        ugdk::script::VirtualObj vdt = ugdk::script::VirtualObj(proxy_.wrapper());
-        vdt.set_value(scene);
         std::list<ugdk::script::VirtualObj> args;
-        args.push_back(vdt);
+
+        SceneProxy* sceneproxy = dynamic_cast<SceneProxy*>(scene);
+        if (sceneproxy != NULL) {
+            args.push_back( sceneproxy->get_proxy_vobj() );
+        }
+        else {
+            ugdk::script::VirtualObj vscene = ugdk::script::VirtualObj(proxy_.wrapper());
+            vscene.set_value<Scene*>(scene);
+            args.push_back( vscene );
+        }
+
         if(proxy_["OnSceneAdd"])
             ( proxy_ | "OnSceneAdd" )(args);
     }
