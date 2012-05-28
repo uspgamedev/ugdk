bd4c75d0dd0dc5c39e7d36a1270e9f920bf9eba9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-05-27 21:59:06 -0300

    GenericCollisionLogic implemented.

---
+++ src/pyramidworks/collision/genericcollisionlogic.h
@@ -0,0 +1,31 @@
+#ifndef PYRAMIDWORKS_COLLISION_GENERICCOLLISIONLOGIC_H_
+#define PYRAMIDWORKS_COLLISION_GENERICCOLLISIONLOGIC_H_
+
+#include <functional>
+#include <pyramidworks/collision/collisionlogic.h>
+
+namespace pyramidworks {
+namespace collision {
+
+/// \class GenericCollisionLogic genericcollisionlogic.h "pyramidworks/collision/genericcollisionlogic.h"
+/// CollisionLogic implementation using std::tr1::function.
+class GenericCollisionLogic {
+  public:
+    GenericCollisionLogic(std::tr1::function<void (void*)> func)
+        : func_(func) {}
+    ~GenericCollisionLogic() {}
+
+    /// Method that is called when a collision happens.
+    /** @param data The data attribute of an CollisionObject. */
+    void Handle(void *data) {
+        func_(data);
+    }
+
+  private:
+    std::tr1::function<void (void*)> func_;
+};
+
+} // namespace collision
+} // namespace pyramidworks
+
+#endif /* PYRAMIDWORKS_COLLISION_GENERICCOLLISIONLOGIC_H_ */
