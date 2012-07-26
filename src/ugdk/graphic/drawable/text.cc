13587a095a17d640cfe5fdb5ac599cf626b1cd9c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-07-26 01:29:09 -0300

    Text now respects Mirror and Color.

--- src/ugdk/graphic/drawable/text.cc
+++ src/ugdk/graphic/drawable/text.cc
@@ -4,12 +4,15 @@
 #include <ugdk/graphic/drawable/text.h>
 
 #include <ugdk/base/engine.h>
+#include <ugdk/base/types.h>
 #include <ugdk/graphic/videomanager.h>
 #include <ugdk/math/frame.h>
 #include <ugdk/graphic/font.h>
 
 namespace ugdk {
 namespace graphic {
+
+using ugdk::Color;
 
 static Vector2D GetStringSize(const std::wstring& string, Font* font) {
     double width = 0, height = font->GetLetterSize(L'\n').y;
@@ -60,36 +63,43 @@ const Vector2D& Text::size() const {
 void Text::Draw(double dt) {
     const Modifier& mod = VIDEO_MANAGER()->CurrentModifier();
     if(!mod.visible()) return;
-
-    // TODO: respect Mirror and Color
-
-    // DOES NOTHING WITH TIME! TODO?
-    Font::IdentType ident = font_->ident();
+    
+    static Color FANCY_COLORS[3] = {
+        Color(1.000000, 1.000000, 1.000000), // 255, 255, 255
+        Color(0.831372, 0.666666, 0.000000), // 212, 170,   0
+        Color(0.333333, 0.266666, 0.000000)};//  85,  68,   0
+		
+	Font::IdentType ident = font_->ident();
 
     glEnable(GL_TEXTURE_2D);
     glListBase(font_->id());
 
-    /*Frame bounds = VIDEO_MANAGER()->virtual_bounds();
-    if (position.x > bounds.right() || position.y > bounds.bottom() ||
-        position.x + width_ < bounds.left() || position.y + height_ < bounds.top() ) {
-        return true;
-    }*/
-    
-    static double FANCY_COLORS[3][3] = {
-        {1.000000, 1.000000, 1.000000}, // 255, 255, 255
-        {0.831372, 0.666666, 0.000000}, // 212, 170,   0
-        {0.333333, 0.266666, 0.000000}};//  85,  68,   0
-
-
-    int fancy_line_number = 0;
-    if (font_->IsFancy()) fancy_line_number = 2;
-
     glPushMatrix();
+	
+	// TODO: combine the hotspot and mirror matrices.
+
+    // Hotspot
     glTranslated(-hotspot_.x, -hotspot_.y, 0.0);
+
+    // horizontal flip
+    if(mod.mirror() & MIRROR_HFLIP) {
+		glTranslated(size_.x, 0.0, 0.0);
+        glScaled(-1.0, 1.0, 1.0);
+	}
+
+    // vertical flip
+    if(mod.mirror() & MIRROR_VFLIP) {
+		glTranslated(0.0, size_.y, 0.0);
+        glScaled(1.0, -1.0, 1.0);
+	}
+	
+	int fancy_line_number = 0;
+    if (font_->IsFancy()) fancy_line_number = 2;
     for(; fancy_line_number >= 0; fancy_line_number--) {
         glTranslated(-1.0, -1.0, 0);
         glPushMatrix();
-        glColor3dv(FANCY_COLORS[fancy_line_number]);
+        Color final_color = mod.color() * FANCY_COLORS[fancy_line_number];
+        glColor4dv(final_color.val);
         for(size_t i = 0; i < message_.size(); ++i) {
             if(i != 0) glTranslated( 0.0, line_height_, 0.0);
             if(message_[i].length() > 0) {
