ed639dbc8a938ed7660403d7617662606c95235b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-07-28 19:44:51 -0300

    GCC uses tr1/header, everything else is just header.

--- src/ugdk/portable/tr1.h
+++ src/ugdk/portable/tr1.h
@@ -2,12 +2,10 @@
 #ifndef UGDK_PORTABLE_TR1_H_
 #define UGDK_PORTABLE_TR1_H_
 
-#include <ugdk/config/config.h>
-
-#ifdef WIN32
-#define FROM_TR1(header) <header>
-#else
+#ifdef __GNUC__
 #define FROM_TR1(header) <tr1/header>
+#else
+#define FROM_TR1(header) <header>
 #endif
 
 #endif /* UGDK_PORTABLE_TR1_H_ */
