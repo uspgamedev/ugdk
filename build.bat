f8d2269b9295ae45751fb30ab0f0775fa5f9e860
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-08-14 18:49:33 -0300

    Fix README.txt not found when using ugdk as external.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if not exist ugdk.vcxproj (cmake -DLINK_TYPE=STATIC .)
+if not exist libugdk.vcxproj (cmake .)
 
 cmake --build . --target libugdk --config Debug
 cmake --build . --target libugdk --config Release
