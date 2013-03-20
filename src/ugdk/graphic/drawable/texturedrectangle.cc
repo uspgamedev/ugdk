6b18e46807ef7362d4c406cd534d3e93261d1f48
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-03-20 19:48:40 -0300

    Using glm for  textured rectangle draw skip.

--- src/ugdk/graphic/drawable/texturedrectangle.cc
+++ src/ugdk/graphic/drawable/texturedrectangle.cc
@@ -4,7 +4,6 @@
 
 //#define GLM_SWIZZLE 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 #include <ugdk/base/engine.h>
 #include <ugdk/graphic/texture.h>
@@ -15,7 +14,7 @@
 
 namespace ugdk {
 namespace graphic {
-
+    
 TexturedRectangle::TexturedRectangle(Texture* texture) : size_(math::Integer2D(texture->width(), texture->height())), texture_(texture) {
     vertexbuffer_ = opengl::VertexBuffer::CreateDefault();
     uvbuffer_ = opengl::VertexBuffer::CreateDefault();
@@ -31,24 +30,17 @@ TexturedRectangle::~TexturedRectangle() {
 
 void TexturedRectangle::Draw(const Geometry& geometry, const VisualEffect& effect) const {
     Geometry final_geometry(geometry * Geometry(math::Vector2D(-hotspot_), size_));
-    float M[16];
-    final_geometry.AsMatrix4x4(M);
-    glm::mat4 mat(M[0], M[1], M[2], M[3], M[4], M[5], M[6], M[7], M[8], M[9], M[10], M[11], M[12], M[13], M[14], M[15]);
+    glm::mat4 mat = final_geometry.as_mat4();
     glm::vec4 left = mat * glm::vec4(0.0, 0.0, 0.0, 1.0), right = mat * glm::vec4(1.0, 1.0, 0.0, 1.0);
 
     if(left.x > 1 || left.y > 1 || right.x < -1 || right.y < -1)
         return;
-    /*
-    if(glm::any(glm::greaterThan(left.xy(), glm::vec2(1.0, 1.0))) || 
-        glm::any(glm::lessThan(right.xy(), glm::vec2(-1.0, -1.0)))) {
-            return;
-    }*/
     // Use our shader
     opengl::ShaderProgram::Use shader_use(VIDEO_MANAGER()->default_shader());
 
     // Send our transformation to the currently bound shader, 
     // in the "MVP" uniform
-    shader_use.SendGeometry(final_geometry);
+    shader_use.SendGeometry(mat);
     shader_use.SendEffect(effect);
 
     // Bind our texture in Texture Unit 0
