5673de952549b13ca9efbffcb04facac205f4138
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-06-28 17:54:48 -0300

    Finish example 1.

--- examples/01/main.cc
+++ examples/01/main.cc
@@ -1,3 +1,17 @@
+#include <ugdk/system/engine.h>
+#include <ugdk/action/scene.h>
+#include <ugdk/input/module.h>
+
 int main(int argc, char* argv[]) {
+    ugdk::system::Initialize();
+    ugdk::action::Scene* scene = new ugdk::action::Scene();
+    scene->AddTask([scene](double dt) {
+        if(ugdk::input::manager()->KeyDown(ugdk::input::K_ESCAPE))
+            scene->Finish();
+        return true; // Keep the task alive
+    });
+    ugdk::system::PushScene(scene);
+    ugdk::system::Run();
+    ugdk::system::Release();
     return 0;
 }
