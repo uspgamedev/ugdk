650e36d59cd05b4c9d1d5f39d191f9f1fd27da8f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-08-11 01:22:43 -0300

    Add operator array for Vector2D.

--- test/src/Box.cc
+++ test/src/Box.cc
@@ -1,18 +1,13 @@
 #include "gtest/gtest.h"
 #include <ugdk/structure/box.h>
+#include <ugdk/math/vector2D.h>
 #include <array>
-
-std::array<double, 2> makeArray(double a, double b) {
-    std::array<double, 2> result;
-    result[0] = a;
-    result[1] = b;
-    return result;
-}
 
 TEST(Box, CopyConstruct) {
     using namespace ugdk::structure;
+    using ugdk::math::Vector2D;
 
-    Box<2> b1(makeArray(0.5, 1.5), makeArray(1.0, 2.0));
+    Box<2> b1(Vector2D(0.5, 1.5), Vector2D(1.0, 2.0));
     Box<2> c(b1);
     EXPECT_EQ(b1.min_coordinates(), c.min_coordinates());
     EXPECT_EQ(b1.max_coordinates(), c.max_coordinates());
@@ -21,8 +16,8 @@ TEST(Box, CopyConstruct) {
 TEST(Box, Update) {
     using namespace ugdk::structure;
 
-    Box<2> b1(makeArray(0.5, 1.5), makeArray(1.0, 2.0));
-    Box<2> b2(makeArray(0.0, 1.5), makeArray(0.5, 2.0));
+    Box<2> b1(Vector2D(0.5, 1.5), Vector2D(1.0, 2.0));
+    Box<2> b2(Vector2D(0.0, 1.5), Vector2D(0.5, 2.0));
 
     EXPECT_NE(b1.min_coordinates(), b2.min_coordinates());
     EXPECT_NE(b1.max_coordinates(), b2.max_coordinates());
