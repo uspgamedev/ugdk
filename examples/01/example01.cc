59250d41ee0e7bfd8afe38288f9527cf652556ab
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-10-09 00:12:07 -0300

    Fix example 01.

--- examples/01/example01.cc
+++ examples/01/example01.cc
@@ -1,14 +1,15 @@
 #include <ugdk/system/engine.h>
 #include <ugdk/action/scene.h>
-#include <ugdk/input/module.h>
+#include <ugdk/input/events.h>
 
 int main(int argc, char* argv[]) {
     ugdk::system::Initialize();
-    ugdk::action::Scene* scene = new ugdk::action::Scene();
-    scene->AddTask([scene](double dt) {
-        if(ugdk::input::manager()->KeyDown(ugdk::input::K_ESCAPE))
-            scene->Finish();
-    });
+    ugdk::action::Scene* scene = new ugdk::action::Scene;
+    scene->event_handler().AddListener<ugdk::input::KeyPressedEvent>(
+        [scene](const ugdk::input::KeyPressedEvent& ev) {
+            if(ev.scancode == ugdk::input::Scancode::ESCAPE)
+                scene->Finish();
+        });
     ugdk::system::PushScene(scene);
     ugdk::system::Run();
     ugdk::system::Release();
