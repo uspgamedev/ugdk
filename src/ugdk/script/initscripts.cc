1b27de1336d29db413401bbd99a39930852e8337
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-12-09 19:26:59 -0200

    Fix initscripts when no swig is present.

--- src/ugdk/script/initscripts.cc
+++ src/ugdk/script/initscripts.cc
@@ -5,10 +5,12 @@
 #include <ugdk/script/languages/lua/luawrapper.h>
 #include <ugdk/script/languages/python/pythonwrapper.h>
 
+#ifdef UGDK_SWIG_ENABLED
 void force_link_function_ugdk_internal(void) { 
     extern int UGDK_MODULES_HEARTBEAT; 
     UGDK_MODULES_HEARTBEAT = 1;
 }
+#endif // UGDK_SWIG_ENABLED
 
 namespace ugdk {
 namespace script {
