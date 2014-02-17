4c9a39c7d8156e7895d1c9daa05410d1111d27bf
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-02-17 16:48:50 -0300

    Log function send to logcat on android.

--- src/ugdk/debug/log.cc
+++ src/ugdk/debug/log.cc
@@ -1,6 +1,13 @@
 #include <ugdk/debug/log.h>
+#include <ugdk/system/config.h>
 
 #include <cstdio>
+
+#ifdef ANDROID
+#include <jni.h>
+#include <string.h>
+#include <android/log.h>
+#endif
 
 namespace ugdk {
 namespace debug {
@@ -10,8 +17,12 @@ void Log(LogLevel level, const std::string& message) {
 }
 
 void Log(LogLevel level, const std::string& owner, const std::string& message) {
+#ifdef ANDROID
+    __android_log_print(ANDROID_LOG_INFO,owner.c_str(),message.c_str());
+#else
     FILE* out = (level < ERROR) ? stderr : stdout;
     fprintf(out, "%s %s - %s\n", owner.c_str(), ConvertLogToString(level), message.c_str());
+#endif
 }
 
 }  // namespace debug
