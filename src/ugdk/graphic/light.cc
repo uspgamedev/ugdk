2f83fd0332dbe9744bf3d7db65db7cfc61bbf439
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-08-31 02:23:26 -0300

    Fix Light

--- src/ugdk/graphic/light.cc
+++ src/ugdk/graphic/light.cc
@@ -2,52 +2,64 @@
 
 #include <ugdk/internal/opengl.h>
 #include <ugdk/graphic/module.h>
-#include <ugdk/graphic/geometry.h>
+#include <ugdk/graphic/canvas.h>
 #include <ugdk/graphic/visualeffect.h>
-#include <ugdk/graphic/opengl/shaderprogram.h>
-#include <ugdk/graphic/opengl/shaderuse.h>
-#include <ugdk/graphic/opengl/vertexbuffer.h>
 
 namespace ugdk {
 namespace graphic {
 
-Light::Light() : dimension_(1.0, 1.0) {
-    static const GLfloat buffer_data[] = { 
-        -1.0f,-1.0f, 
-        -1.0f, 1.0f,
-         1.0f,-1.0f, 
-         1.0f, 1.0f 
+namespace {
+    struct VertexXY {
+        float x, y;
     };
-    vertexbuffer_ = opengl::VertexBuffer::Create(sizeof(buffer_data), GL_ARRAY_BUFFER, GL_STATIC_DRAW);
+}
+
+Light::Light()
+    : dimension_(1.0, 1.0)
+    , data_(4, sizeof(VertexXY), false)
+{
+    set_dimension(dimension_);
+}
+
+Light::~Light() {}
+
+void Light::set_dimension(const ugdk::math::Vector2D& dimension) {
+    dimension_ = dimension;
+    VertexData::Mapper mapper(data_);
     {
-        opengl::VertexBuffer::Bind bind(*vertexbuffer_);
-        opengl::VertexBuffer::Mapper mapper(*vertexbuffer_);
-
-        GLfloat *indices = static_cast<GLfloat*>(mapper.get());
-        if (indices)
-            memcpy(indices, buffer_data, sizeof(buffer_data));
+        VertexXY* p = mapper.Get<VertexXY>(0);
+        p->x = -dimension_.x;
+        p->y = -dimension_.y;
     }
+    {
+        VertexXY* p = mapper.Get<VertexXY>(1);
+        p->x =  dimension_.x;
+        p->y = -dimension_.y;
+    }
+    {
+        VertexXY* p = mapper.Get<VertexXY>(2);
+        p->x = -dimension_.x;
+        p->y =  dimension_.y;
+    }
+    {
+        VertexXY* p = mapper.Get<VertexXY>(3);
+        p->x =  dimension_.x;
+        p->y =  dimension_.y;
+    }
+
 }
 
-Light::~Light() {
-    delete vertexbuffer_;
-}
+void Light::Draw(Canvas &canvas) {
+    const opengl::ShaderProgram* old_program = canvas.shader_program();
 
-void Light::Draw(const Geometry& geometry) {
-    // Use our shader
-    opengl::ShaderProgram* shader = graphic::manager()->light_shader();
-    opengl::ShaderUse shader_use(shader);
+    canvas.ChangeShaderProgram(graphic::manager()->light_shader());
+    canvas.SendUniform("decayment", 2.4f);
+    canvas.SendUniform("minimum_radius", 0.05f);
+    canvas.PushAndCompose(VisualEffect(color_));
+    canvas.SendVertexData(data_, VertexType::VERTEX, 0);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-    shader_use.SendUniform("decayment", 2.4f);
-    shader_use.SendUniform("minimum_radius", 0.05f);
-    shader_use.SendGeometry(geometry * Geometry(math::Vector2D(), dimension_));
-    shader_use.SendEffect(VisualEffect(color_));
-
-    // 1rst attribute buffer : vertices
-    shader_use.SendVertexBuffer(vertexbuffer_, opengl::VERTEX, 0);
-
-    // Draw the triangle !
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4); // 12*3 indices starting at 0 -> 12 triangles
+    canvas.ChangeShaderProgram(old_program);
 }
 
 }  // namespace graphic
