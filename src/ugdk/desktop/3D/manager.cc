b51c8feddf9663d582bbe68fd0b56a19cae3853c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-10-02 00:59:34 -0300

    Load plugins file and a lackluster ugdk path integration in ogre.

--- src/ugdk/desktop/3D/manager.cc
+++ src/ugdk/desktop/3D/manager.cc
@@ -5,8 +5,13 @@
 #include <ugdk/desktop/windowsettings.h>
 #include <ugdk/math/integer2D.h>
 #include <ugdk/debug/log.h>
+#include <ugdk/system/engine.h>
 
+#ifdef OGRE_STATIC_LIB
+// FIXME: HELP, WE'RE HARD CODING THIS
 #define OGRE_STATIC_GL
+#endif
+
 #include <OgreStaticPluginLoader.h>
 #include <OgreSceneManager.h>
 #include <OgreStringConverter.h>
@@ -35,8 +40,12 @@ Manager::~Manager() {
 }
 
 bool Manager::Initialize() {
+
+    std::string mResourcesCfg = system::ResolvePath("resources" OGRE_BUILD_SUFFIX ".cfg");
+    std::string mPluginsCfg = system::ResolvePath("plugins" OGRE_BUILD_SUFFIX ".cfg");
+
     // Create the Root
-    root_ = new Ogre::Root("");
+    root_ = new Ogre::Root(mPluginsCfg);
     
     // Load static plugins
     static_loader_ = new Ogre::StaticPluginLoader();
@@ -44,7 +53,7 @@ bool Manager::Initialize() {
     
     // Setup resources
     Ogre::ConfigFile cf;
-    cf.load("resources.cfg");
+    cf.load(mResourcesCfg);
     // Go through all sections & settings in the file
     Ogre::ConfigFile::SectionIterator seci = cf.getSectionIterator();
     Ogre::String secName, typeName, archName;
@@ -56,7 +65,7 @@ bool Manager::Initialize() {
             typeName = i->first;
             archName = i->second;
             Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-                archName, typeName, secName);
+                system::ResolvePath(archName), typeName, secName);
         }
     }
     
