3790deffaf66aa6d30dc389a6f2d5de0245919a9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-12-24 17:15:15 -0200

    Added object listeners to EventHandler.

--- modules/ugdk-core/include/ugdk/system/eventhandler.h
+++ modules/ugdk-core/include/ugdk/system/eventhandler.h
@@ -5,7 +5,7 @@
 #include <typeinfo>
 #include <typeindex>
 #include <functional>
-#include <vector>
+#include <forward_list>
 #include <memory>
 #include <unordered_map>
 #include <algorithm>
@@ -19,17 +19,22 @@ class IListener { public: virtual ~IListener() {} };
 template <class Event>
 class Listener : public IListener {
   public:
-    template<typename Handler>
-    Listener(Handler handler) : handler_(handler) {}
+    virtual void Handle(const Event& ev) = 0;
+};
 
-    void Handle(const Event& ev) {
+template <class Event>
+class FunctionListener : public Listener<Event> {
+  public:
+    template<typename Handler>
+    FunctionListener(Handler handler) : handler_(handler) {}
+
+    void Handle(const Event& ev) override {
         handler_(ev);
     }
 
   private:
     std::function<void (const Event&)> handler_;
 };
-
 
 class EventHandler {
   public:
@@ -48,9 +53,19 @@ class EventHandler {
         return AddRawListener(typeid(Event), new Listener<Event>(handler));
     }
 
-    IListener* AddRawListener(const std::type_index& type, IListener* listener) {
-        event_handlers_[type].emplace_back(listener);
-        return listener;
+    IListener* AddRawListener(const std::type_index& type, std::shared_ptr<IListener> listener) {
+        event_handlers_[type].emplace_front(listener);
+        return listener.get();
+    }
+
+    /** Adds an object to listen to all events dispatched to this EventHandler.
+        The object must implement the Listener<T> interface for the events it wants to track.
+        @param listener The object to dispatch events to. This class does not delete the
+                        received pointer and should be notified when it becomes invalid via
+                        RemoveObjectListener.
+    */
+    void AddObjectListener(IListener* listener) {
+        object_listeners_.push_front(listener);
     }
 
     // Remove listeners
@@ -59,18 +74,18 @@ class EventHandler {
         auto handlers = event_handlers_.find(type);
         if(handlers == event_handlers_.end()) return;
 
-        handlers->second.erase(
-            std::remove_if(handlers->second.begin(), 
-                           handlers->second.end(),
-                           [listener](const std::shared_ptr<IListener>& val) {
-                               return val.get() == listener;
-                           }),
-            handlers->second.end());
+        handlers->second.remove_if([listener](const std::shared_ptr<IListener>& val) {
+            return val.get() == listener;
+        });
     }
 
     template<class Event>
     void RemoveListener(IListener* listener) {
         RemoveListener(typeid(Event), listener);
+    }
+
+    void RemoveObjectListener(IListener* listener) {
+        object_listeners_.remove(listener);
     }
 
     // Raising events
@@ -84,12 +99,19 @@ class EventHandler {
             assert(specific_listener);
             specific_listener->Handle(ev);
         }
+
+        for (IListener* listener : object_listeners_) {
+            if (Listener<Event>* specific_listener = dynamic_cast<Listener<Event>*>(listener)) {
+                specific_listener->Handle(ev);
+            }
+        }
     }
 
   private:
-    typedef std::vector< std::shared_ptr<IListener> > ListenerVector;
+    typedef std::forward_list< std::shared_ptr<IListener> > ListenerVector;
 
     std::unordered_map<std::type_index, ListenerVector> event_handlers_;
+    std::forward_list<IListener*> object_listeners_;
 };
 
 } // namespace system
