7a122397abba3cdffef5ab779061d5bfa989c35e
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-12-27 18:59:34 -0200

    Add joystick status methods.

--- modules/ugdk-core/include/ugdk/input/joystick.h
+++ modules/ugdk-core/include/ugdk/input/joystick.h
@@ -12,12 +12,79 @@ struct _SDL_Joystick;
 namespace ugdk {
 namespace input {
 
+class AxisStatus {
+  public: 
+    AxisStatus(int16 raw);
+
+    int16 raw() const;
+    int16 MaximumValue() const;
+    int16 MinimumValue() const;
+    double Percentage() const;
+
+  private:
+    int16 raw_;
+};
+
+class HatStatus {
+  public:
+    HatStatus(int raw);
+
+    int raw() const;
+    bool IsCentered() const;
+    bool IsUp() const;
+    bool IsRight() const;
+    bool IsDown() const;
+    bool IsLeft() const;
+    bool IsRightUp() const;
+    bool IsRightDown() const;
+    bool IsLeftUp() const;
+    bool IsLeftDown() const;
+
+    bool HasUp() const;
+    bool HasRight() const;
+    bool HasDown() const;
+    bool HasLeft() const;
+
+  private:
+    int raw_;
+};
+
 class Joystick {
   public:
     ~Joystick();
 
     /// Checks if the joystick is currently connected.
     bool IsConnected() const;
+
+    /// Get the number of general axis controls.
+    int NumAxes() const;
+
+    /// Get the number of trackballs.
+    /** Trackballs have only relative motion events associated
+    *   with them and their state cannot be polled.
+    */
+    int NumTrackballs() const;
+
+    /// Get the number of POV hats.
+    int NumHats() const;
+
+    /// Get the number of buttons.
+    int NumButtons() const;
+
+    /// Checks if the given button is currently being held.
+    bool IsDown(int button) const;
+
+    /// Checks if there's currently no interaction with the given button.
+    bool IsUp(int button) const;
+
+    /// Get the ball axis changes since last frame.
+    math::Integer2D GetBallDelta(int ballid) const;
+
+    /// Get the current state of a POV hat.
+    HatStatus GetHatStatus(int hat) const;
+
+    /// Get the current state of an axis control.
+    AxisStatus GetAxisStatus(int axis) const;
 
   private:
     Joystick(_SDL_Joystick*);
--- modules/ugdk-core/src/input/joystick.cc
+++ modules/ugdk-core/src/input/joystick.cc
@@ -1,9 +1,80 @@
 #include <ugdk/input/joystick.h>
 
+#include <ugdk/system/exceptions.h>
+
 #include "SDL_joystick.h"
 
 namespace ugdk {
 namespace input {
+
+AxisStatus::AxisStatus(int16 raw)
+    : raw_(raw)
+{}
+
+int16 AxisStatus::raw() const { return raw_; }
+
+int16 AxisStatus::MaximumValue() const {
+    return 32767;
+}
+
+int16 AxisStatus::MinimumValue() const {
+    return -32768;
+}
+
+double AxisStatus::Percentage() const {
+    if (raw_ < 0) {
+        return -raw_ / static_cast<double>(MinimumValue());
+    } else {
+        return raw_ / static_cast<double>(MaximumValue());
+    }
+}
+
+HatStatus::HatStatus(int raw)
+    : raw_(raw)
+{}
+
+int HatStatus::raw() const { return raw_; }
+
+bool HatStatus::IsCentered() const {
+    return raw_ == SDL_HAT_CENTERED;
+}
+bool HatStatus::IsUp() const {
+    return raw_ == SDL_HAT_UP;
+}
+bool HatStatus::IsRight() const {
+    return raw_ == SDL_HAT_RIGHT;
+}
+bool HatStatus::IsDown() const {
+    return raw_ == SDL_HAT_DOWN;
+}
+bool HatStatus::IsLeft() const {
+    return raw_ == SDL_HAT_LEFT;
+}
+bool HatStatus::IsRightUp() const {
+    return raw_ == SDL_HAT_RIGHTUP;
+}
+bool HatStatus::IsRightDown() const {
+    return raw_ == SDL_HAT_RIGHTDOWN;
+}
+bool HatStatus::IsLeftUp() const {
+    return raw_ == SDL_HAT_LEFTUP;
+}
+bool HatStatus::IsLeftDown() const {
+    return raw_ == SDL_HAT_LEFTDOWN;
+}
+
+bool HatStatus::HasUp() const {
+    return (raw_ & SDL_HAT_UP) != 0;
+}
+bool HatStatus::HasRight() const {
+    return (raw_ & SDL_HAT_RIGHT) != 0;
+}
+bool HatStatus::HasDown() const {
+    return (raw_ & SDL_HAT_DOWN) != 0;
+}
+bool HatStatus::HasLeft() const {
+    return (raw_ & SDL_HAT_LEFT) != 0;
+}
 
 Joystick::Joystick(SDL_Joystick* joystick)
     : sdljoystick_(joystick)
@@ -17,6 +88,46 @@ Joystick::~Joystick() {
 
 bool Joystick::IsConnected() const {
     return SDL_JoystickGetAttached(sdljoystick_) == SDL_TRUE;
+}
+
+int Joystick::NumAxes() const {
+	return SDL_JoystickNumAxes(sdljoystick_);
+}
+
+int Joystick::NumTrackballs() const {
+	return SDL_JoystickNumBalls(sdljoystick_);
+}
+
+int Joystick::NumHats() const {
+	return SDL_JoystickNumHats(sdljoystick_);
+}
+
+int Joystick::NumButtons() const {
+	return SDL_JoystickNumButtons(sdljoystick_);
+}
+
+bool Joystick::IsDown(int button) const {
+    return SDL_JoystickGetButton(sdljoystick_, button) > 0;
+}
+
+bool Joystick::IsUp(int button) const {
+    return SDL_JoystickGetButton(sdljoystick_, button) == 0;
+}
+
+math::Integer2D Joystick::GetBallDelta(int ballid) const {
+    math::Integer2D result;
+    if (0 != SDL_JoystickGetBall(sdljoystick_, ballid, &result.x, &result.y)) {
+        throw system::InvalidOperation("Invalid ballid");
+    }
+    return result;
+}
+
+HatStatus Joystick::GetHatStatus(int hat) const {
+    return HatStatus(SDL_JoystickGetHat(sdljoystick_, hat));
+}
+
+AxisStatus Joystick::GetAxisStatus(int axis) const {
+    return AxisStatus(SDL_JoystickGetAxis(sdljoystick_, axis));
 }
 
 } // namespace input
