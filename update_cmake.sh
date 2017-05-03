9685663f2f4cdc2a77229a2b60a5094c070e9a92
josealvim <jose.alvim@usp.br>
2017-05-03 15:40:56 -0300

    Running into problems

---
+++ update_cmake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function find_files() {
+    echo "SET(GENERATED_SRC"
+    find modules/$1/src -type f | sort
+    echo ")"
+    echo "SET(GENERATED_HEADERS"
+    find modules/$1/include -type f | sort
+    echo ")"
+}
+
+# CORE SOURCE
+find_files ugdk-core > modules/ugdk-core/generated_src_list.cmake
+
+# 2D SOURCE
+find_files ugdk-2d > modules/ugdk-2d/generated_src_list.cmake
+
+# 3D SOURCE
+find_files ugdk-3d > modules/ugdk-3d/generated_src_list.cmake
