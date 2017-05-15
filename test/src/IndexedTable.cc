420d4d3a13551e0c10250f0750db0db2e7e8d310
josealvim <jose.alvim@usp.br>
2017-05-15 16:00:39 -0300

    Add test to iterate over table

--- test/src/IndexedTable.cc
+++ test/src/IndexedTable.cc
@@ -13,15 +13,14 @@ TEST(IndexableTable, MapMethods) {
     IndexableTable<int> table;
     table.Add("first", std::unique_ptr<int>(a));
     table.Add("second", std::unique_ptr<int>(b));
-    
     table.Add("third", std::unique_ptr<int>(c));
     table.Remove("third");
-    
+
     EXPECT_EQ(2u, table.size());
     EXPECT_EQ(a, table.Search("first"));
     EXPECT_EQ(b, table.Search("second"));
     EXPECT_EQ(nullptr, table.Search("third"));
-    
+
 }
 
 struct TestData {
@@ -37,7 +36,7 @@ TEST(IndexableTable, DeleteElements) {
         table.Add("first", ugdk::MakeUnique<TestData>(&count));
         table.Add("second", ugdk::MakeUnique<TestData>(&count));
         table.Add("third", ugdk::MakeUnique<TestData>(&count));
-        
+
         table.Remove("third");
         EXPECT_EQ(2, count);
     }
@@ -45,16 +44,16 @@ TEST(IndexableTable, DeleteElements) {
 }
 
 TEST(IndexableTable, Id) {
-    int *a = new int; 
+    int *a = new int;
     int *b = new int;
     IndexableTable<int> table;
     table.Add("first", std::unique_ptr<int>(a));
     table.Add("second", std::unique_ptr<int>(b));
-    
+
     int id1 = table.MakeIndex("first");
     int id2 = table.MakeIndex("second");
     EXPECT_EQ(-1, table.MakeIndex("third"));
-    
+
     EXPECT_EQ(a, table.Get(id1));
     EXPECT_EQ(b, table.Get(id2));
 }
@@ -64,6 +63,32 @@ TEST(IndexableTable, InvalidOperation) {
         IndexableTable<int> table;
         table.Add("null", nullptr);
         EXPECT_FALSE(table.ReleaseIndex(5));
+    }
+}
+
+TEST(IndexableTable, IterateTableConst) {
+    IndexableTable<int> table;
+    std::vector<int*> v;
+    int *a = new int;
+    int *b = new int;
+    int *c = new int;
+
+    table.Add("first"     , std::unique_ptr<int>(a));
+    table.Add("second", std::unique_ptr<int>(b));
+    table.Add("third"    , std::unique_ptr<int>(c));
+
+    for (const auto &i : table)
+        v.push_back(i.second.get());
+
+    EXPECT_EQ(3u, v.size());
+
+    bool test;
+    for (const auto &i : table) {
+        test = true;
+        test =       (i.second.get() != v[0])
+                    &&(i.second.get() != v[1])
+                    &&(i.second.get() != v[2]);
+        EXPECT_FALSE(test);
     }
 }
 
