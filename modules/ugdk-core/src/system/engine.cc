c1e9431a46140e68830f6422d8e05b24758014f2
josealvim <jose.alvim@usp.br>
2017-06-19 15:07:37 -0300

    Change engine not to create a new manager
    But rather see if on is active and then get it from the getter function

--- modules/ugdk-core/src/system/engine.cc
+++ modules/ugdk-core/src/system/engine.cc
@@ -231,7 +231,7 @@ void Run() {
         while(profile_data_list_.size() > 10)
             profile_data_list_.pop_front();
 
-        if (configuration_.limit_frame_time_with_sleep && std::unique_ptr<time::Manager>(new time::Manager )) {
+        if (configuration_.limit_frame_time_with_sleep && time::is_active()) {
             auto frame_time = time::manager().TimeElapsedInFrame();
             if (frame_time < configuration_.target_frame_time)
                 SDL_Delay(configuration_.target_frame_time - frame_time);
