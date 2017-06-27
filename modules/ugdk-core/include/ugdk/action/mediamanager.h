71da9d95ed623ab34b87e57ea3701f4ad3548e59
Rubens.Bolgheroni <rubens.bolgheroni@gmail.com>
2017-06-27 11:19:26 -0300

    Fixed my error
    I had left the most important part out. "The const"

--- modules/ugdk-core/include/ugdk/action/mediamanager.h
+++ modules/ugdk-core/include/ugdk/action/mediamanager.h
@@ -22,12 +22,12 @@ class MediaManager {
             it->Update(dt);
     }
 
-    void AddPlayer(std::shared_ptr<MediaPlayer>& player) {
+    void AddPlayer(const std::shared_ptr<MediaPlayer>& player) {
         players_.insert(player);
         player->manager_ = this;
     }
 
-    void RemovePlayer(std::shared_ptr<MediaPlayer>& player) {
+    void RemovePlayer(const std::shared_ptr<MediaPlayer>& player) {
         players_.erase(player);
         player->manager_ = nullptr;
     }
