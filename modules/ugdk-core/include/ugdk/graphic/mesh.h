98b1a59b087e13e9604fa02582ffa816c8697032
Kazuo256 <kazuo256@gmail.com>
2017-10-09 18:04:24 -0300

    [WIP] Design Mesh API

--- modules/ugdk-core/include/ugdk/graphic/mesh.h
+++ modules/ugdk-core/include/ugdk/graphic/mesh.h
@@ -1,1 +1,42 @@
-// continue here
+
+#ifndef UGDK_GRAPHIC_MESH_H_
+#define UGDK_GRAPHIC_MESH_H_
+
+#include <ugdk/graphic/vertexdata.h>
+
+#include <glm/vec2.hpp>
+
+#include <vector>
+
+namespace ugdk {
+namespace graphic {
+
+class Canvas;
+
+template <typename Position, typename Texel>
+class Mesh {
+  public:
+    struct Vertex {
+      Position  pos;
+      Texel     tex;
+    };
+    Mesh(const DrawMode& the_mode, const std::vector<Vertex>& the_vtx_data);
+    Mesh(const Mesh& rhs) = delete;
+    Mesh& operator=(const Mesh& rhs) = delete;
+    Mesh(Mesh&& rhs);
+    Mesh& operator=(Mesh&& rhs);
+  private:
+    friend Canvas& operator<<(Canvas &canvas, const Mesh<Position, Texel> &mesh);
+    DrawMode    mode_;
+    VertexData  vtx_data_;
+};
+
+using MeshXYUV = Mesh<glm::vec2, glm::vec2>;
+
+template <typename Position, typename Texel>
+Canvas& operator<<(Canvas &canvas, const Mesh<Position, Texel> &mesh);
+
+} // namespace ugdk
+} // namespace graphic
+
+#endif // UGDK_GRAPHIC_MESH_H_
